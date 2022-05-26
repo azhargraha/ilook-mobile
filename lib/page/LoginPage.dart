7542ac309b2294f86fe66e70e78e33440c053bb2
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-05-27 00:50:58 +0700

    fix view overflow

--- lib/page/LoginPage.dart
+++ lib/page/LoginPage.dart
@@ -1,29 +1,30 @@
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-  class LoginPage extends StatelessWidget {
-    const LoginPage({Key? key}) : super(key: key);
-    
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-      Size screen = MediaQuery.of(context).size;
-      return SafeArea(
-        child: Scaffold(
-          extendBodyBehindAppBar: true,
-          body: Container(
-            width: screen.width,
-            height: screen.height,
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                Column(
+  @override
+  Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        body: Container(
+          width: screen.width,
+          height: screen.height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              SingleChildScrollView(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
                       margin: EdgeInsets.symmetric(vertical: 10),
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                       width: screen.width * 0.8,
                       child: Image.asset('assets/images/logo.png'),
                     ),
@@ -32,30 +33,25 @@ import 'package:flutter/material.dart';
                       child: Text(
                         'Welcome to iLook',
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 48
-                        ),
+                            fontWeight: FontWeight.bold, fontSize: 22),
                       ),
                     ),
                     Container(
-                      margin: EdgeInsets.only(bottom:35),
+                      margin: EdgeInsets.only(bottom: 35),
                       child: Text(
                         'Sign in to continue',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.grey),
+                        style: TextStyle(fontSize: 14, color: Colors.grey),
                       ),
-                    ), 
+                    ),
                     TextFieldContainer(
                       child: TextField(
                         decoration: InputDecoration(
-                          border: InputBorder.none,
-                          icon: Icon(
-                            Icons.person,
-                            color: Colors.black,
-                          ),
-                          hintText: 'Username'
-                        ),
+                            border: InputBorder.none,
+                            icon: Icon(
+                              Icons.person,
+                              color: Colors.black,
+                            ),
+                            hintText: 'Username'),
                       ),
                     ),
                     TextFieldContainer(
@@ -75,14 +71,13 @@ import 'package:flutter/material.dart';
                       margin: EdgeInsets.only(bottom: 15),
                       width: screen.width * 0.8,
                       child: TextButton(
-                        onPressed: (){},
+                        onPressed: () {},
                         child: Text(
                           'Forgot Password?',
                           style: TextStyle(
-                            color: Colors.grey[800],
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                          ),
+                              color: Colors.grey[800],
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
@@ -91,63 +86,60 @@ import 'package:flutter/material.dart';
                       height: screen.height * 0.06,
                       width: screen.width * 0.8,
                       child: TextButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.black),
-                          foregroundColor: MaterialStateProperty.all(Colors.white),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          'Sign in',
-                          style: TextStyle(
-                            fontSize: 20
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.white),
                           ),
-                        )
-                      ),
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(fontSize: 18),
+                          )),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text(
-                          "Don't have an account ? ",
-                          style: TextStyle(color: Colors.grey[600], fontSize: 18)
-                        ),
+                        Text("Don't have an account ? ",
+                            style: TextStyle(
+                                color: Colors.grey[600], fontSize: 14)),
                         GestureDetector(
                           onTap: () {},
-                          child: Text(
-                            'Sign up now',
-                            style: TextStyle(color: Colors.grey[800], fontSize: 18, fontWeight: FontWeight.bold)
-                          ),
+                          child: Text('Sign up now',
+                              style: TextStyle(
+                                  color: Colors.grey[800],
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold)),
                         ),
                       ],
                     )
                   ],
                 ),
-
-              ],
-            ),
-
+              ),
+            ],
           ),
         ),
-      );
-    }
-
+      ),
+    );
   }
-  class TextFieldContainer extends StatelessWidget {
-    final Widget? child;
-    const TextFieldContainer({Key? key, this.child}) : super(key: key);
+}
 
-    @override
-    Widget build(BuildContext context){
-      Size size = MediaQuery.of(context).size;
-      return Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        width: size.width * 0.8,
-        decoration: BoxDecoration(
+class TextFieldContainer extends StatelessWidget {
+  final Widget? child;
+  const TextFieldContainer({Key? key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
           border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5)
-          ),
-        child: child,
-      );
-    }
+          borderRadius: BorderRadius.circular(5)),
+      child: child,
+    );
   }
+}
