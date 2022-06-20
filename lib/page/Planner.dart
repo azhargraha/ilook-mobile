3760e59dd0c69f127dce8582761783bd64b180e9
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-06-21 00:04:42 +0700

    solve merge error

--- lib/page/Planner.dart
+++ lib/page/Planner.dart
@@ -1,92 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:planner/models/plan.dart';
-import 'package:planner/page/DatePicker.dart';
-import 'package:planner/page/TaskTimeline.dart';
-import 'package:planner/page/TaskTitle.dart';
-import 'package:planner/models/plan.dart';
+import 'package:ilook/models/plan.dart';
+import 'package:ilook/page/DatePicker.dart';
+import 'package:ilook/page/TaskTimeline.dart';
+import 'package:ilook/page/TaskTitle.dart';
+import 'package:ilook/models/plan.dart';
+
 final List<Map<String, dynamic>> stringList = [
   {
-            'time' : '10.00 am',
-            'title': 'ke Bandung',
-            'slot' : '10.00 - 12.00 am',
-            'tlColor' : Colors.redAccent,
-            'bgColor' : Colors.white,
-          },
-          {
-            'time' : '12.00 am',
-            'title': 'ke Bandung',
-            'slot' : '12.00 - 13.00 am',
-            'tlColor' : Colors.lightBlue,
-            'bgColor' : Colors.white,
-          },
+    'time': '10.00 am',
+    'title': 'ke Bandung',
+    'slot': '10.00 - 12.00 am',
+    'tlColor': Colors.redAccent,
+    'bgColor': Colors.white,
+  },
+  {
+    'time': '12.00 am',
+    'title': 'ke Bandung',
+    'slot': '12.00 - 13.00 am',
+    'tlColor': Colors.lightBlue,
+    'bgColor': Colors.white,
+  },
 ];
-class Planner extends StatefulWidget {
-  
 
+class Planner extends StatefulWidget {
   @override
   State<Planner> createState() => _PlannerState();
 }
 
 class _PlannerState extends State<Planner> {
- 
   @override
   Widget build(BuildContext context) {
     final detialList = stringList;
     return SafeArea(
-      child: Scaffold (
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers:[
-          _buildAppBar(context),
-          SliverToBoxAdapter(
-
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                )
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [DatePicker(),TaskTitle()],
-        
+        child: Scaffold(
+            backgroundColor: Colors.white,
+            body: CustomScrollView(slivers: [
+              _buildAppBar(context),
+              SliverToBoxAdapter(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      )),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [DatePicker(), TaskTitle()],
+                  ),
                 ),
               ),
-            ),
-        
-          detialList == null ?
-          SliverFillRemaining(
-            child: Container(
-              color: Colors.white,
-              child: Center(child: Text('No Planner has been added',
-            style: TextStyle(color: Colors.grey, fontSize: 18))))  
-        )
-        : 
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-            (_, index) => TaskTimeLine(detialList[index]),
-            childCount: detialList.length),
-          ),
-        ])
-    ));
-}
+              detialList == null
+                  ? SliverFillRemaining(
+                      child: Container(
+                          color: Colors.white,
+                          child: Center(
+                              child: Text('No Planner has been added',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 18)))))
+                  : SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                          (_, index) => TaskTimeLine(detialList[index]),
+                          childCount: detialList.length),
+                    ),
+            ])));
+  }
 
-  Widget _buildAppBar(BuildContext context){
+  Widget _buildAppBar(BuildContext context) {
     return SliverAppBar(
-      expandedHeight: 90,
-      backgroundColor: Colors.white,
-      leading: IconButton(
-        onPressed:() => Navigator.of(context).pop(),
-        icon: Icon(Icons.arrow_back_ios,color: Colors.black,),
-        iconSize: 20,
+        expandedHeight: 90,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          iconSize: 20,
         ),
         actions: [
           Icon(
-            Icons.more_vert, color: Colors.black,
+            Icons.more_vert,
+            color: Colors.black,
             size: 40,
           )
         ],
@@ -95,18 +92,15 @@ class _PlannerState extends State<Planner> {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Planner title',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                )),
+              Text('Planner title',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
               SizedBox(height: 5),
-              Text(
-                'Bandung',
-                style: TextStyle(fontSize: 12, color: Colors.black
-                )
-          )],
-        ),
-    ));
+              Text('Bandung',
+                  style: TextStyle(fontSize: 12, color: Colors.black))
+            ],
+          ),
+        ));
   }
 }
--- lib/page/PlannerList.dart
+++ lib/page/PlannerList.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:planner/widgets/packageCard.dart';
-import 'package:planner/models/Package.dart';
+import 'package:ilook/widget/packageCard.dart';
+import 'package:ilook/models/Package.dart';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
-
 class PackagePage extends StatefulWidget {
-  const PackagePage({ Key? key }) : super(key: key);
+  const PackagePage({Key? key}) : super(key: key);
 
   @override
   State<PackagePage> createState() => _PackagePageState();
 }
 
 class _PackagePageState extends State<PackagePage> {
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,17 +22,17 @@ class _PackagePageState extends State<PackagePage> {
           backgroundColor: Colors.white,
           foregroundColor: Colors.black,
         ),
-        body: 
-        // FutureBuilder<List<Package>>(
-        //   future: fetchPackage(http.Client()),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.hasData){
-        //       return PackageList(packages: snapshot.data!);
-        //     }else {
-        //       return Center(child: CircularProgressIndicator());
-        //     }
-        //   })
-        PackageList(),
+        body:
+            // FutureBuilder<List<Package>>(
+            //   future: fetchPackage(http.Client()),
+            //   builder: (context, snapshot) {
+            //     if (snapshot.hasData){
+            //       return PackageList(packages: snapshot.data!);
+            //     }else {
+            //       return Center(child: CircularProgressIndicator());
+            //     }
+            //   })
+            PackageList(),
       ),
     );
   }
@@ -50,27 +48,30 @@ class _PackagePageState extends State<PackagePage> {
 //   return parsed.map<Package>((json)=> Package.fromJson(json)).toList();
 // }
 
-
 class PackageList extends StatelessWidget {
   // final List<Package> packages;
-  const PackageList({ Key? key}) : super(key: key);
-  
+  const PackageList({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final List<Map> myProducts =
-      List.generate(100000, (index) => {"id": index, "name": "Product $index"})
-          .toList();
+    final List<Map> myProducts = List.generate(
+        100000, (index) => {"id": index, "name": "Product $index"}).toList();
     return ListView.builder(
-      itemCount: myProducts.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () => Navigator.pushNamed(context, '/PlacePlannerList',
-            arguments: Package(
-                title: 'Paket $index',
-                description: 'Deskripsi Paket $index',
-                thumbnailUrl: 'https://www.planetware.com/photos-large/INA/indonesia-beaches-of-bali.jpg')),
-          child: PackageCard(title: 'Judul $index', description: 'Ini deskripsi yang panjang buat $index', thumbnailUrl: 'https://www.planetware.com/photos-large/INA/indonesia-beaches-of-bali.jpg'),
-        );
-      });
+        itemCount: myProducts.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, '/PlacePlannerList',
+                arguments: Package(
+                    title: 'Paket $index',
+                    description: 'Deskripsi Paket $index',
+                    thumbnailUrl:
+                        'https://www.planetware.com/photos-large/INA/indonesia-beaches-of-bali.jpg')),
+            child: PackageCard(
+                title: 'Judul $index',
+                description: 'Ini deskripsi yang panjang buat $index',
+                thumbnailUrl:
+                    'https://www.planetware.com/photos-large/INA/indonesia-beaches-of-bali.jpg'),
+          );
+        });
   }
 }
